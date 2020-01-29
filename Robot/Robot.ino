65e5c4ba805fecc51a580ee04a3c11a2b6af0916
Emilsboq <emil.olsson1@jgy.se>
2020-01-29 12:29:56 +0100

    DRV8833-Styrning

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,9 +1,9 @@
 const int controlPin1 = 2;
-const int controlPin2 = 3;
-const int enablePin = 9;
+const int controlPin2 = 6;
+const int enablePin = 0;
 const int directionSwitchPin = 4;
-const int onOffSwitchStateSwitchPin = 5;
 const int potPin = A0;
+const int onOffSwitchStateSwitchPin = 5;
 
 int onOffSwitchState = 0;
 int previousOnOffSwitchState = 0;
@@ -14,64 +14,55 @@ int motorEnabled = 0;
 int motorSpeed = 0;
 int motorDirection = 1;
 
-void setup()
-{
+
+
+void setup() {
   // put your setup code here, to run once:
   pinMode(directionSwitchPin, INPUT);
-  pinMode(onOffSwitchStateSwitchPin, INPUT);
   pinMode(controlPin1, OUTPUT);
   pinMode(controlPin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
 
   digitalWrite(enablePin, LOW);
+
 }
 
-void loop()
-{
+void loop() {
   // put your main code here, to run repeatedly:
-  onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
-  delay(1);
   directionSwitchState = digitalRead(directionSwitchPin);
-  motorSpeed = analogRead(potPin) / 4;
-
-  if (onOffSwitchState != previousOnOffSwitchState)
-  {
-    if (onOffSwitchState == HIGH)
-    {
+  motorSpeed = analogRead(potPin)/4;
+  if (onOffSwitchState != previousOnOffSwitchState) {
+    if (onOffSwitchState == HIGH) {
       motorEnabled = !motorEnabled;
     }
   }
 
-  if (directionSwitchState != previousDirectionSwitchState)
-  {
-    if (directionSwitchState == HIGH)
-    {
+  if (directionSwitchState != previousDirectionSwitchState) {
+    if (directionSwitchState == HIGH) {
       motorDirection = !motorDirection;
     }
   }
 
-  if (motorDirection == 1)
-  {
+  if (motorDirection == 1) {
     digitalWrite(controlPin1, HIGH);
     digitalWrite(controlPin2, LOW);
   }
 
-  else
-  {
+  else {
     digitalWrite(controlPin1, LOW);
     digitalWrite(controlPin2, HIGH);
   }
 
-  if (motorEnabled == 1)
-  {
+  if (motorEnabled == 1) {
     analogWrite(enablePin, motorSpeed);
   }
 
-  else
-  {
+  else {
     analogWrite(enablePin, 0);
   }
 
   previousDirectionSwitchState = directionSwitchState;
   previousOnOffSwitchState = onOffSwitchState;
+
+
 }
